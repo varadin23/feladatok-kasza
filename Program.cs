8c096ed7c80fb0a7955c4222906c5d8f820c5798
Váradi Niki <74589652+varadin23@users.noreply.github.com>
2022-04-28 10:39:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    public class Program
+    {
+        public class adatok
+        {
+            
+            public string nev;
+            public int elsolepes;
+            public int masodiklepes;
+            public int suly;
+            public int magassag;
+
+            List<adatok> adatoklista = new List<adatok>();
+           
+                
+       
+
+        }
+        static void Main(string[] args)
+        {
+           StreamReader sr = new StreamReader("balkezesek.csv");
+           sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string nev= sr.ReadLine();
+                string[] adatoklista = nev.Split(' ');
+                adatok a = new adatok();
+                a.elsolepes = Convert.ToInt32(adatoklista[0]);
+                a.masodiklepes = Convert.ToInt32(adatoklista[1]);
+                a.suly = Convert.ToInt32(adatoklista[1]);
+                a.magassag = Convert.ToInt32(adatoklista[1]);
+            }
+            sr.Close();
+
+            Console.WriteLine(inchtocm(32));
+            Console.ReadKey();
+            
+        }
+
+        public static double inchtocm(int inch)
+        {
+            return inch*2.54;
+        }
+    }
+}
